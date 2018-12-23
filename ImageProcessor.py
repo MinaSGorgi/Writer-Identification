282c3cb93b16e8ac401ef3f4b4f6fb2c5960785c
Hassan Saied <hsaied17@gmail.com>
2018-12-23 22:27:26 +0200

    Added PairWise Vector Generator

--- ImageProcessor.py
+++ ImageProcessor.py
@@ -5,30 +5,31 @@ from features.LPQ import LPQ
 from skimage import io
 from pathlib import Path
 import pickle
+from collections import defaultdict
 
 
-def processImages(datasetPath, mode, n_max=float('inf')):
-    feature_vectors = []
-    labels = []
+
+def processImages(datasetPath, mode, max_writers=float('inf')):
     writerDict = readDataSet(datasetPath)
+    writerFeatureVectorsDict = defaultdict(list)
     for index, writer in enumerate(writerDict):
-        if index == n_max:
+        if index == max_writers:
             break
         for writerImagePath in writerDict[writer]:
             image = io.imread(writerImagePath, as_gray=True)
             preprocessedImages = preprocessImage(image)
             for preprocessedFragment in preprocessedImages:
                 if mode == 'LBP':
-                    feature_vectors.append(LBP(preprocessedFragment))
+                    writerFeatureVectorsDict[writer].append(LBP(preprocessedFragment))
                 elif mode == 'LPQ':
-                    feature_vectors.append(LPQ(preprocessedFragment))
+                    writerFeatureVectorsDict[writer].append(LPQ(preprocessedFragment))
                 else:
                     raise RuntimeError
-                labels.append(writer)
     with open('features.obj', 'wb') as features_file:
-        pickle.dump((feature_vectors, labels), features_file)
+        pickle.dump(writerFeatureVectorsDict, features_file)
+
+
+
 
 
 processImages(Path.home() / 'Documents' / 'PatternProject' / 'iamDB', 'LPQ', 5)
-with open('features.obj', 'rb') as features_file:
-    feature_vectors, labels = pickle.load(features_file)
